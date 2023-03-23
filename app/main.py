60e3e9c3d550213d2475f7d92d09a2449514e7cb
Joao Maranhao <joaofelipemaranhao@gmail.com>
2023-03-23 19:11:58 -0300

    refactor: change root endpoint to redirect to api docs

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="Vet Appointment Manager",
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", include_in_schema=False)
+def docs():
+    return RedirectResponse(url="/docs")
