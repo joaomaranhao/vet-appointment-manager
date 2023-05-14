9dcb112a5a22aa47cd7b5e34e0d85e2cb6ba15a1
João Maranhão <joaofelipemaranhao@gmail.com>
2023-05-14 16:58:34 -0300

    feat: :sparkles:
    criar cliend crud

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -56,3 +56,47 @@ def delete_veterinarian(db: Session, veterinarian_id: int):
     db.delete(db_veterinarian)
     db.commit()
     return db_veterinarian
+
+
+def get_client(db: Session, client_id: int):
+    return db.query(models.Client).filter(models.Client.id == client_id).first()
+
+
+def get_clients(db: Session, skip: int = 0, limit: int = 100):
+    clients = db.query(models.Client).offset(skip).limit(limit).all()
+    return clients
+
+
+def create_client(db: Session, client: schemas.ClientCreate):
+    db_client = models.Client(
+        nome=client.nome,
+        cpf=client.cpf,
+        telefone=client.telefone,
+        email=client.email,
+        endereco=client.endereco,
+    )
+    db.add(db_client)
+    db.commit()
+    db.refresh(db_client)
+    return db_client
+
+
+def update_client(
+    client_id: int, db: Session, client: schemas.ClientUpdate = Body(..., embed=True)
+):
+    db_client = db.query(models.Client).filter(models.Client.id == client_id).first()
+    if not db_client:
+        raise HTTPException(status_code=404, detail="Client not found")
+    update_data = client.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_client, key, value)
+    db.commit()
+    db.refresh(db_client)
+    return db_client
+
+
+def delete_client(db: Session, client_id: int):
+    db_client = db.query(models.Client).filter(models.Client.id == client_id).first()
+    db.delete(db_client)
+    db.commit()
+    return db_client
