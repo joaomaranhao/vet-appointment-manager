30367e2209c6988849381f5920506d87d13bf26f
João Maranhão <joaofelipemaranhao@gmail.com>
2023-05-14 17:30:51 -0300

    feat: :sparkles: criar rotas clients

--- backend/app/main.py
+++ backend/app/main.py
@@ -85,6 +85,54 @@ def delete_veterinarian(veterinarian_id: int, db: Session = Depends(get_db)):
     return crud.delete_veterinarian(db=db, veterinarian_id=veterinarian_id)
 
 
+@app.get("/clients/", response_model=list[schemas.Client])
+def read_clients(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    clients = crud.get_clients(db, skip=skip, limit=limit)
+    return clients
+
+
+@app.get("/clients/{client_id}", response_model=schemas.Client)
+def read_client(client_id: int, db: Session = Depends(get_db)):
+    db_client = crud.get_client(db, client_id=client_id)
+    if db_client is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Client not found",
+        )
+    return db_client
+
+
+@app.post("/clients/", response_model=schemas.Client)
+def create_client(client: schemas.ClientCreate, db: Session = Depends(get_db)):
+    return crud.create_client(db=db, client=client)
+
+
+@app.put("/clients/{client_id}", response_model=schemas.Client)
+def update_client(
+    client_id: int,
+    client: schemas.ClientUpdate,
+    db: Session = Depends(get_db),
+):
+    db_client = crud.get_client(db, client_id=client_id)
+    if db_client is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Client not found",
+        )
+    return crud.update_client(client_id=client_id, db=db, client=client)
+
+
+@app.delete("/clients/{client_id}", response_model=schemas.Client)
+def delete_client(client_id: int, db: Session = Depends(get_db)):
+    db_client = crud.get_client(db, client_id=client_id)
+    if db_client is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Client not found",
+        )
+    return crud.delete_client(db=db, client_id=client_id)
+
+
 @app.get("/", include_in_schema=False)
 def docs():
     return RedirectResponse(url="/docs")
